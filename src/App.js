073d842d1fdb22d0a6226acded53468764ec8901
Jordan Boudreau <jordboudreau@gmail.com>
2019-01-06 22:27:06 -0800

    add css and layout of game

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const NUM_BOXES = 16;
+
+const Box = ({color}) => {
+  const style = {
+    backgroundColor: color,
+    padding: '5px',
+    width: '10vw',
+    height: '10vw',
+    margin: '5px'
+  }
+
+  return <div style={style} />;
+}
+
 class App extends Component {
+  
   render() {
+    const boxes = this.shuffleBoxes().map((color, i) => (
+      <Box key={i} color={color} />
+    ));
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <p>Render 16 tiles, 8x2 pairs</p>
+          <p>Tile state: showTile</p>
+          <p>Tile props: color</p>
+          <p>App funcs: flip, isMatched, shuffleColors</p>
+        </div>
+        <div className='gameboard'>
+          {boxes}
+        </div>
       </div>
     );
   }
+
+  shuffleBoxes(){
+    // To shuffle an array a of n elements (indices 0..n-1):
+    // for i from n - 1 downto 1 do
+    //      j = random integer with 0 <= j <= i
+    //      exchange a[j] and a[i]
+    let boxes = this.props.colors;
+    console.log('hello')
+    for(let i=boxes.length-1; i>=1; i--){
+      console.log('hi')
+      let j = Math.floor(Math.random() * NUM_BOXES);
+      let box = boxes[j];
+      let holdBox = boxes[i];
+      boxes[i] = box;
+      boxes[j] = holdBox;
+    }
+
+    return boxes;
+  }
+
 }
+
+App.defaultProps  = {
+  colors: ['#00b894', '#00cec9', '#0984e3', '#6c5ce7', '#ffeaa7', '#fab1a0', '#ff7675', '#fd79a8', '#00b894', '#00cec9', '#0984e3', '#6c5ce7', '#ffeaa7', '#fab1a0', '#ff7675', '#fd79a8']
+}
+
 
 export default App;
---
+++ src/box.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+    static defaultProps = {
+        
+    }
+    render(){
+        return (
+            <div className='box'></div>
+        )
+    }
+}
+
+export default Box;
